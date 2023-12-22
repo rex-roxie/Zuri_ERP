4c645e97d17d7f5d8cee7f2ea072de93a8889bb3
rexroxie <rex.roxie@outlook.com>
2023-12-22 14:53:11 -0500

    Get info from drivers docs

--- src/firebase.js
+++ src/firebase.js
@@ -20,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 // Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+export const db = getFirestore(app);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { auth } from '../firebase';
+import React, { useEffect, useState } from 'react';
+import { auth, db } from '../firebase';
 import { signOut } from 'firebase/auth';
+import { collection, getDocs } from "firebase/firestore";
 import { useNavigate } from 'react-router-dom';
+
+const getDrivers = async () => {
+  const querySnapshot = await getDocs(collection(db, "drivers"));
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id);
+  });
+}
 
 const Home = () => {
   const navigate = useNavigate();
+  const [drivers, setDrivers] = useState([]);
+
+  const getDrivers = async () => {
+    const querySnapshot = await getDocs(collection(db, "drivers"));
+      // doc.data() is never undefined for query doc snapshots
+    setDrivers(querySnapshot.docs.map((doc) => ({...doc.data(), id: doc.id})));
+  }
+
+  getDrivers();
 
   const logout = () => {
     signOut(auth);
@@ -30,6 +48,11 @@ const Home = () => {
         <button onClick={name}>Change Name</button>
         <button onClick={password}>Change Password</button>
         <button onClick={email}>Change Email</button>
+        <section>
+          {drivers.map((driver) => {
+            return <div className='driver'>{driver.first_name} {driver.last_name}</div>
+          })}
+        </section>
     </div>
   )
 }
