675289aaf52f3c545679605eb8bfc70d3b279d07
rexroxie <rex.roxie@outlook.com>
2024-01-06 21:56:19 -0500

    Be able to update driver email

--- src/pages/DriverInfo.js
+++ src/pages/DriverInfo.js
@@ -1,7 +1,12 @@
+import { doc, updateDoc } from 'firebase/firestore';
 import React, {useState} from 'react'
+import { db } from '../firebase';
 
 function DriverInfo(props) {
   const [showDriverInfo, setShowDriverInfo] = useState('none');
+  const [values, setValues] = useState({
+    email: ""
+  });
 
   const driverInfo = () => {
     if (showDriverInfo === "none") {
@@ -11,10 +16,32 @@ function DriverInfo(props) {
     }
   }
 
+  const updateDriverEmail = async (event) => {
+    event.preventDefault();
+    await updateDoc(doc(db, "drivers", props.driver.id), {
+      email: values.email
+    }).then(() => {
+      setValues({
+        ...values, email: ""
+      });
+    });
+  }
+
+  const onChange = (event) => {
+    setValues({
+        ...values,
+        [event.target.name]: event.target.value,
+    });
+  }
+
     return (
         <div>
-            <div className='driver' onClick={driverInfo}>{props.driver.first_name} {props.driver.last_name}</div> 
-            <div style={{display: showDriverInfo}}>{props.driver.email}</div>
+            <div className='driver' onClick={driverInfo} name="email">{props.driver.first_name} {props.driver.last_name}</div> 
+            <div style={{display: showDriverInfo}}>
+              {props.driver.email}
+              <input placeholder={props.driver.email} type='text' name="email" onChange={onChange} value={values.email} />
+              <button type='submit' onClick={updateDriverEmail}>Update</button>
+            </div>
         </div>
   )
 }
